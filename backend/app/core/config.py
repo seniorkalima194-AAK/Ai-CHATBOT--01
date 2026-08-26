25801fbc1c177c7f0e683fc9bf33389f8e7aa94b
abdully <seniorkalima194@gmail.com>
2026-08-26 19:32:06 +0300

    Improve readability in config.py
    Add missing line breaks for better readability.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -61,5 +61,7 @@ class Settings(BaseSettings):
 @lru_cache(maxsize=1)
 def get_settings() -> Settings:
     return Settings()
+
+
 # This runs during startup and fails immediately for missing/invalid
 settings.settings = get_settings()
